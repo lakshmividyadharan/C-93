c8c0a978bbdb068f101f4ca1c07ee7ff7c03e5ea
lakshmividyadharan <76952303+lakshmividyadharan@users.noreply.github.com>
2021-09-16 16:48:45 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,61 @@
+
+//ADD YOUR FIREBASE LINKS
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGbxeTO2EphlJkBqgxviIErsu5Mf9h6cg",
+  authDomain: "kwittertry-76f09.firebaseapp.com",
+  databaseURL: "https://kwittertry-76f09-default-rtdb.firebaseio.com",
+  projectId: "kwittertry-76f09",
+  storageBucket: "kwittertry-76f09.appspot.com",
+  messagingSenderId: "207171058926",
+  appId: "1:207171058926:web:99c228494094132250c7ff"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+  user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "kwitter.html";
+}
